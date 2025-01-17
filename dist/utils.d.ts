c491d1116092556f96e1b195c3f7da2b6dab1299
John Keiser <john@johnkeiser.com>
2025-01-17 13:07:04 -0800

    Actually compile it ...

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,2 +1,5 @@
 import axios from 'axios';
 export declare function createSiApiClient(): axios.AxiosInstance;
+export declare function getApiToken(): string;
+export declare function getApiUrl(): string;
+export declare function getWebUrl(): string;
